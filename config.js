35fc52540797d98492f63dcc67d2496cf5976480
trung281003 <85343514+trung281003@users.noreply.github.com>
2021-06-04 17:46:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Tâmmm à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Hươnggg à !',
+    introDesc: `leaguage of legend`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: ' master yi',
+    desc: 'yasuo ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Hôm nào rảnh cùng anh đi chơi nhé !!!!.',
+    question:'Tối làm trận lol đê <3',
+    btnReply: 'ok bựn iuuu <3',
+    reply: 'tối chơi nhé <33333333',
+    mess: 't biết m sẽ đồng ý mà kkk',
+    messDesc: '9h chơi nhé!!!!.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://facebook.com/thanhtrung281003' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
